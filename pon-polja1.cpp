780bb9a09ef9ff98a7059c22de07cb29466aab9d
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2020-02-24 23:07:05 +0100

    Add files via upload

---
+++ pon-polja1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n,a[n],b,t;
+	cout<<"upisite velicinu polja: ";
+	cin>>n;
+	a[n];
+	cout<<"upisi "<< n <<" brojeva: "<<endl;
+	for(int i=0;i<=n;i++){
+	 		cin>>a[i];
+		
+		
+		
+		}
+	cout<<"do koliko biste htjeli da se ispisu brojevi: "<<endl;
+	cin>>t;
+	for(int b=0;b<=t;b++){
+		
+		cout<<a[b]<<endl;
+	
+	}
+	
+	
+	system("pause");
+	return 0;
+}
